a11dd876bcde569f5d621b01426f993128201b94
YangHun <pitahano@gmail.com>
2019-01-13 18:13:56 +0900

    add project folder

---
+++ sandbox/Assets/CharacterAI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAI : MonoBehaviour {
+
+    enum Action { Pickup = 1, Wave = 2 }
+
+    public NavMeshAgent agent;
+
+    public float timer = 0.0f;
+    public Animator animator;
+    public Vector3 src;
+    public Vector3 dir;
+
+    public bool isMoving = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!animator.GetBool("Grounded"))
+        {
+            animator.SetBool("Grounded", true);
+            agent.isStopped = true;
+        }   
+    }
+
+    private void Update()
+    {
+        if (!animator.GetBool("Grounded") || !animator.GetBool("isIdle"))
+            return;
+
+
+        if (agent.isStopped)
+        {
+            src = transform.position;
+            dir = new Vector3(Random.Range(-1.0f, 1.0f), 0.0f, Random.Range(-1.0f, 1.0f)) * 5.0f;
+            agent.isStopped = false;
+            animator.SetFloat("MoveSpeed", 1.0f);
+            agent.SetDestination(src + dir);
+        }
+
+        else if (timer > 5.0f || agent.remainingDistance <= agent.stoppingDistance)
+        {
+            animator.SetFloat("MoveSpeed", 0.0f);
+            agent.isStopped = true;
+
+            Action next = (Action)Random.Range(1, 2);
+
+            switch (next)
+            {
+                case Action.Wave:
+                    animator.SetTrigger("Wave");
+                    break;
+                case Action.Pickup:
+                    animator.SetTrigger("Pickup");
+                    break;
+            }
+
+            animator.SetBool("isIdle", false);
+
+            timer = 0.0f;
+            
+            dir.Normalize();
+            
+        }
+        
+        timer += Time.deltaTime;        
+    }
+}
---
+++ sandbox/Assets/MyCharacter.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyCharacter : MonoBehaviour
+{
+
+	public Text CoinText;
+
+#region Data
+	bool bSavedBefore;
+	int nPurchases;
+	int nCoin;
+#endregion
+
+	Rigidbody _rigidbody;
+
+	// Use this for initialization
+	void Start()
+	{
+		_rigidbody = GetComponentInChildren<Rigidbody>();
+
+
+		if (PlayerPrefs.HasKey("IsSaved"))
+		{
+			nPurchases = PlayerPrefs.GetInt("nPurchases");
+			nCoin = PlayerPrefs.GetInt("nCoin");
+			Debug.Log(nCoin);
+			transform.localScale += new Vector3(nPurchases, nPurchases, nPurchases);
+			bSavedBefore = true;
+		}
+		else
+		{
+			nCoin = 5;
+			nPurchases = 0;
+			bSavedBefore = false;
+		}
+
+		CoinText.text = "Coin X " + nCoin;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (_rigidbody.gameObject.transform.position.y > 200)
+			_rigidbody.velocity = Physics.gravity;
+	}
+
+	public void OnClickButtonUpgrade()
+	{
+		if (nCoin >= 1)
+		{
+	
+			_rigidbody.AddForce(Vector3.up * 100.0f * 9.8f);
+			transform.localScale += new Vector3(1.0f, 1.0f, 1.0f);
+
+			nPurchases++;
+			nCoin--;
+
+			CoinText.text = "Coin X " + nCoin;
+
+			//set data to PlayerPrefs.
+			if (!bSavedBefore)
+			{
+				PlayerPrefs.SetInt("IsSaved", 1);
+				bSavedBefore = true;
+			}
+
+			PlayerPrefs.SetInt("nPurchases", nPurchases);
+			PlayerPrefs.SetInt("nCoin", nCoin);
+		}
+	}
+
+	public void OnClickButtonReset()
+	{
+		transform.localScale = new Vector3(10f, 10f, 10f);
+		nPurchases = 0;
+
+		if (bSavedBefore)
+			PlayerPrefs.SetInt("nPurchases", nPurchases);
+	}
+
+	public void OnClickButtonPurchase ()
+	{
+		nCoin += 10;
+		CoinText.text = "Coin X " + nCoin;
+		PlayerPrefs.SetInt("nCoin", nCoin);
+	}
+}
