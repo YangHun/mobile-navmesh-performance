435ef46e0897f7d7c806c56d2a8a95d66b013662
eunjung <ejyang@cookapps.com>
2019-01-16 19:01:34 +0900

    WIP performance check; finished checking static object issue, pooling object, checking number of vertice under 5k

---
+++ sandbox/Assets/LobbyObjectPool.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CookApps {
+
+	public class LobbyObjectPool : MonoBehaviour {
+
+		//--------------------------------------------------------------------------------//
+        //-----------------------------------FIELD----------------------------------------//
+        //--------------------------------------------------------------------------------//
+        //------------------- Inspector ------------------//
+        [SerializeField]
+        int currentLevel;
+
+        [SerializeField]
+        List <GameObject> Levels;
+
+        
+
+        //------------------- public ------------------//
+
+
+        //------------------- protected ------------------//
+
+
+        //------------------- private ------------------//
+
+
+        //--------------------------------------------------------------------------------//
+        //------------------------------------PROPERTY------------------------------------//
+        //--------------------------------------------------------------------------------//
+
+
+        //--------------------------------------------------------------------------------//
+        //------------------------------------METHOD--------------------------------------//
+        //--------------------------------------------------------------------------------//
+
+
+		// Use this for initialization
+		void Start () {
+			for (int i = 0; i < currentLevel; i++) {
+                Levels[i].SetActive (true);
+                StartCoroutine(MakeStaticObjects(Levels[i].transform));
+            }
+		}
+
+        private void RetrieveChildToStatic(Transform parent) {
+            
+            parent.gameObject.isStatic = true;
+
+            if (parent.childCount == 0)
+                return;
+            else{
+                for (int i = 0; i < parent.childCount; i++)
+                    RetrieveChildToStatic(parent.GetChild(i));
+            }
+
+        }
+
+        IEnumerator MakeStaticObjects (Transform root) {
+
+            
+            Animator[] _animator = root.GetComponentsInChildren<Animator>();
+
+            foreach (Animator a in _animator) {
+                a.SetTrigger("Start");
+                 yield return null;
+            }
+            
+
+            yield return new WaitForSeconds(5.0f);
+            Debug.Log ("change non-static to static");
+
+
+            RetrieveChildToStatic(root);
+            yield return null;
+
+            StaticBatchingUtility.Combine(root.gameObject);
+            
+            
+            yield return new WaitForSeconds(5.0f);
+            Debug.Log ("setactive-false to animated objects");
+
+            foreach (Animator a in _animator) {
+                a.gameObject.SetActive(false);
+                yield return null;
+            }
+
+
+        }
+
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+
+
+        //───────────────────────────────────────────────────────────────────────────────────────
+		
+	}
+
+}
---
+++ sandbox/Assets/Scripts/StaticObjectManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticObjectManager : MonoBehaviour {
+
+    //--------------------------------------------------------------------------------//
+    //-----------------------------------FIELD----------------------------------------//
+    //--------------------------------------------------------------------------------//
+    //------------------- Inspector ------------------//
+    [SerializeField]
+    Texture2D targetTexture;
+
+    //------------------- public ------------------//
+
+
+    //------------------- protected ------------------//
+
+
+    //------------------- private ------------------//
+
+
+    //--------------------------------------------------------------------------------//
+    //------------------------------------PROPERTY------------------------------------//
+    //--------------------------------------------------------------------------------//
+
+
+    //--------------------------------------------------------------------------------//
+    //------------------------------------METHOD--------------------------------------//
+    //--------------------------------------------------------------------------------//
+
+
+    // Use this for initialization
+    void Start () {
+        this.gameObject.isStatic = true;
+
+        for (int i = 0; i < transform.childCount; i++) {
+            transform.GetChild(i).gameObject.isStatic = true;
+        }
+
+        StaticBatchingUtility.Combine(this.gameObject);
+
+        StartCoroutine (DynamicChangeStatic());
+    }
+    
+
+    IEnumerator DynamicChangeStatic () {
+
+        for (int i = 0 ; i < 5; i++) {
+            
+            Debug.Log ("wait seconds... "+ i);
+            yield return new WaitForSeconds (1.0f);           
+        }
+
+        for (int i = 0; i < transform.childCount; i++) {
+            if (i%2 ==0)
+                transform.GetChild(i).gameObject.SetActive(false);
+        }
+        
+
+        yield return new WaitForSeconds (5.0f);
+        Debug.Log ("change static objects to non-static");
+
+        this.gameObject.isStatic = false;
+        yield return null;
+        for (int i = 0; i < transform.childCount; i++) {
+            transform.GetChild(i).gameObject.isStatic = false;
+            yield return null;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+
+    //───────────────────────────────────────────────────────────────────────────────────────
+    
+}
